9239013e185aa8356ef63c2cc0ebf84f9ab3146f
Kolja <lasar@rasal.de>
2022-10-23 10:02:15 +0200

    db functions

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 This PHP script collects data about the URL and stores it in an SQLite database and displays it as a chart.
 
+URL to store: `localhost:9090/store.php?ESP=haus_one&temp_1=10&temp_2=29&hum_1=40`
+
 ## `config.php`
 
 
--- store.php
+++ store.php
@@ -1,47 +1,77 @@
 <?php
-echo "<pre>";
 
+echo "<pre>";
+// localhost:9090/store.php?ESP=haus_one&temp_1=10&temp_2=29&hum_1=40
 include 'config.php';
 
 
 
+// get ESP name from URL and remove it from GET array
+$esp = ($_GET['ESP']) ? $_GET['ESP'] : 'haus_one';
+unset($_GET['ESP']);
+// print_r($_GET);
 
-$esp = 'haus_one';
-$sensors = $config['ESP'][$esp]['sensors'];
 
-// delete db file
-unlink($config['db_file']);
+
+
+// delete db file & connect to database
+// unlink($config['db_file']);
 $db = new SQLite3($config['db_file']);
 
+// initialize database
+// initDB($esp);
+insertValues($esp);
 
-initDB($esp, $sensors);
+fetchValues($esp);
+
+/**
+ * fetchValues($esp)
+ * @param string $esp
+ * @return void
+ */
+function fetchValues($esp)
+{
+    global $db;
+    $res = $db->query("SELECT * FROM $esp");
+    $result = [];
+    while ($row = $res->fetchArray()) {
+      $result[] = $row;
+        // echo "{$row[0]} {$row[1]} {$row[2]}\n";
+    }
+    print_r($result);
+
+    // return $result;
+
+}
 
 
-$stm = $db->prepare("INSERT INTO $esp(temp_1, temp_1) VALUES (?, ?)");
-$stm->bindParam(1, $firstName);
-$stm->bindParam(2, $lastName);
-
-$firstName = 'Peter';
-$lastName = 'Novak';
-$stm->execute();
-
-$firstName = 'Lucy';
-$lastName = 'Brown';
-$stm->execute();
-
-$res = $db->query("SELECT * FROM $esp");
-
-while ($row = $res->fetchArray()) {
-    echo "{$row[0]} {$row[1]} {$row[2]}\n";
+/**
+ * insertValues($esp)
+ * @param string $esp
+ * @return void
+ */
+function insertValues($esp)
+{
+    global $db;
+    // get all keys and values from URL as two strings
+    $columns = '('.implode(',', array_keys($_GET)).')';
+    $values = '('.implode(',', $_GET).')';
+    // prepare and execute statement
+    $statement = $db->prepare("insert into $esp" .$columns ." values" .$values);
+    $statement->execute();
 }
 
 
 
 
 
-print_r($esp);
-print_r($sensors);
-print_r($config);
+
+
+
+
+// print_r($esp);
+// print_r($sensors);
+// print_r($config);
 
 
 
@@ -50,20 +80,25 @@ print_r($config);
 
 
 /**
- * 
+ *
  * initDB()
- * 
+ *
  * @param string $esp
  * @param array $sensors
  */
-function initDB($esp, $sensors){
-  global $db;
-  $db->exec("CREATE TABLE IF NOT EXISTS  $esp(id INTEGER PRIMARY KEY AUTOINCREMENT)");  
-  $db->exec("ALTER TABLE  $esp ADD COLUMN date DATETIME DEFAULT CURRENT_TIMESTAMP ");
-  foreach ($sensors as $sensor) {
-    $sensorName = $sensor['name'];
-    $db->exec("ALTER TABLE  $esp ADD COLUMN $sensorName INTEGER NOT NULL DEFAULT '0' ");
-  }
+function initDB($esp)
+{
+    global $db,$config;
+
+    // get all sensors for this ESP from database
+    $sensors = $config['ESP'][$esp]['sensors'];
+
+    $db->exec("CREATE TABLE IF NOT EXISTS  $esp(id INTEGER PRIMARY KEY AUTOINCREMENT)");
+    $db->exec("ALTER TABLE  $esp ADD COLUMN date DATETIME DEFAULT CURRENT_TIMESTAMP ");
+    foreach ($sensors as $sensor) {
+        $sensorName = $sensor['name'];
+        $db->exec("ALTER TABLE  $esp ADD COLUMN $sensorName INTEGER NOT NULL DEFAULT '0' ");
+    }
 }
 
 
@@ -71,4 +106,13 @@ function initDB($esp, $sensors){
 
 
 
- 
+////////////// OLD
+
+
+
+// $stm = $db->prepare("INSERT INTO $esp(temp_1, temp_2) VALUES (?, ?)");
+// $stm->bindParam(1, $firstName);
+// $stm->bindParam(2, $lastName);
+// $firstName = 'Lucy';
+// $lastName = 'Brown';
+// $stm->execute();
