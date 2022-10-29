d89b1e9f42bf3e4f256e5c78c77265bd22ba60d1
Kolja <lasar@rasal.de>
2022-10-29 14:19:07 +0200

    readme

--- readme.md
+++ readme.md
@@ -8,11 +8,31 @@ This PHP script collects data about the URL and stores it in an SQLite database 
 - All configuration data stored in `store/config.json`.
 - Including Pythen script for evaluation
 
+## Install
 
-## Links
+copy the code in a local folder
 
-- [create dummy data](http://localhost:9090/store/assets/dummy.php)
-- [insert values](localhost:9090/store/?ESP=EG_1&WZ_TR=10&WZ_HK1_VL=29&WZ_HK1_RL=40)
-- [initialize database](http://localhost:9090/store/?initDB)  will not delete existing db
+run:  
+`php -S localhost:9090`
 
+### use the following links
+
+visit chart:  
+`http://localhost:9090`
+
+create dummy data:
+`http://localhost:9090/store/assets/dummy.php` 
+
+insert values:  
+`localhost:9090/store/?ESP=EG_1&WZ_TR=10&WZ_HK1_VL=29&WZ_HK1_RL=40`
+
+initialize database: (will not delete existing db)  
+`http://localhost:9090/store/?initDB`
+
+remove database:   
+`http://localhost:9090/store/?removeDB`
+
+
+
+ 
 
--- store/index.php
+++ store/index.php
@@ -32,6 +32,13 @@ elseif (isset($_GET['initDB'])) {
   exit;
 }
 
+// remove database
+elseif (isset($_GET['removeDB'])) {
+  echo '<h2>remove database</h2>';
+  removeDB();
+  exit;
+}
+
 // throw error 
 else {
   echo '<h2>no ESP name in URL</h2>';
@@ -101,21 +108,18 @@ foreach ($sensors as $sensor) {
 $array['date'] = date("Y-m-d H:i:s");
 
 
+
 // make strings from arrays
 //
 $columns = '(' . implode(',', array_keys($array)) . ')';
 $placeholder = '(' . implode(',', $placeholder) . ',?)';
 
 
-// pprint($array, '$array');
-// pprint($columns, '$columns');
-// pprint($placeholder, 'placeholder');
-// pprint(array_values($array), 'array_values($array)');
-
 
 // prepare statement
 //
 $statement = $db->prepare("INSERT INTO $esp" . $columns . " VALUES" . $placeholder);
+
 
 
 // try to execute or catch error
@@ -182,139 +186,16 @@ function initDB() {
   exit;
 }
 
-
-
-//////////////////////////////////  FUNCTIONS  //////////////////////////////////
-
-// // create table
-// //
-// if ($db->exec("CREATE TABLE IF NOT EXISTS $esp(id INTEGER PRIMARY KEY AUTOINCREMENT)")) {
-//   echo "<h2>CREATE TABLE IF NOT EXISTS $esp(id INTEGER PRIMARY KEY AUTOINCREMENT)</h2>";
-// }
-
-// // create date column
-// //
-// if ($db->exec("ALTER TABLE $esp ADD COLUMN date DATETIME DEFAULT CURRENT_TIMESTAMP")) {
-//  echo "<h2>ALTER TABLE $esp ADD COLUMN date DATETIME DEFAULT CURRENT_TIMESTAMP</h2>";
-// }
-
-// // create a column for every sensor
-// //
-// foreach ($sensors as $sensor) {
-//   $sensorName = $sensor['name'];
-//   // if ($db->exec("ALTER TABLE $esp ADD COLUMN $sensorName INTEGER NOT NULL DEFAULT '0' ")) {
-//  if ($db->exec("ALTER TABLE $esp ADD COLUMN $sensorName TEXT ")) {
-//    echo "<h2>ALTER TABLE $esp ADD COLUMN $sensorName TEXT </h2>";
-//   }
-// }
-// echo "<h2>Database initialized</h2>";
-// exit;
-
-
-// /**
-//  * insertValues($esp)
-//  * @param string $esp
-//  * @return void
-//  */
-// function insertValues($esp) {
-//   global $db;
-//   // get all keys and values from URL as two strings
-//   $date = date("Y-m-d H:i:s");
-//   $columns = '(' . implode(',', array_keys($_GET)) . ',date)';
-//   $values = "('" . implode("','", $_GET) . "','" . $date . "')";
-
-//   // echo $columns . "<br>";
-//   // echo $values . "<br>";
-
-//   // prepare and execute statement
-//   $statement = $db->prepare("insert into $esp" . $columns . " values" . $values);
-//   $statement->execute();
-// }
-
-
-
-
-// /**
-//  *
-//  * initDB()
-//  *
-//  * @param string $esp
-//  * @param array $sensors
-//  */
-// function initDB($esp) {
-//   global $db, $config;
-
-//   // get all sensors for this ESP from database
-//   $sensors = $config['ESP'][$esp]['sensors'];
-//   // print_r($sensors);
-
-//   $db->exec("CREATE TABLE IF NOT EXISTS  $esp(id INTEGER PRIMARY KEY AUTOINCREMENT)");
-//   $db->exec("ALTER TABLE  $esp ADD COLUMN date DATETIME DEFAULT CURRENT_TIMESTAMP ");
-//   foreach ($sensors as $sensor) {
-//     $sensorName = $sensor['name'];
-//     $db->exec("ALTER TABLE  $esp ADD COLUMN $sensorName INTEGER NOT NULL DEFAULT '0' ");
-//   }
-// }
-
-
-
-
-
-
-////////////// OLD
-
-
-// prepare and execute statement
-// $statement = $db->prepare("insert into $esp" . $columns . " values" . $values);
-// $statement->execute();
-
-
-
-// echo key($config['ESP']);
-
-// $date = strtolower(date("Y-m-d", strtotime(date('d-m-Y'))));
-// echo $date;
-
-// $tomorrow = mktime(0, 0, 0, date("m"), date("d") + 1, date("Y"));
-
-// $date = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d") + 1, date("Y")));
-// echo $date;
-
-
-
-// $esp = (isset($_GET['ESP'])) ? $_GET['ESP'] : key($config['ESP']);
-
-// $stm = $db->prepare("INSERT INTO $esp(temp_1, temp_2) VALUES (?, ?)");
-// $stm->bindParam(1, $firstName);
-// $stm->bindParam(2, $lastName);
-// $firstName = 'Lucy';
-// $lastName = 'Brown';
-// $stm->execute();
-
-
-
-
-
-
-
-// fetchValues($esp);
-// /**
-//  * fetchValues($esp)
-//  * @param string $esp
-//  * @return void
-//  */
-// function fetchValues($esp)
-// {
-//     global $db;
-
-//     $stmt = $db->prepare("SELECT * FROM $esp WHERE strftime('%Y-%m-%d %H:%M:S', date) BETWEEN  :startDate AND :endDate");
-//     $stmt->bindValue('startDate', '2022-10-23 08:00:15', SQLITE3_TEXT);
-//     $stmt->bindValue('endDate', '2022-10-23 09:00:15', SQLITE3_TEXT);
-
-
-//     $res = $stmt->execute();
-
-//     while ($row = $res->fetchArray()) {
-//         echo "{$row['id']} {$row['date']} {$row['temp_2']} \n";
-//     }
-// }
+/**
+ *
+ * remove database
+ *
+ * store/?removeDB
+ *
+ */
+function removeDB() {
+  global $config, $db;
+  unlink('assets/' . $config['db_file']);
+  echo "<h2>Database removed</h2>";
+  exit;
+}
