78bd0e136e165e7333ffe4da9fb32d508eaedb7b
Kolja <lasar@rasal.de>
2022-10-29 14:04:27 +0200

    wip

--- store/assets/functions.php
+++ store/assets/functions.php
@@ -11,7 +11,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Berlin');
 
-// disable debug output
+// disable debug output globally
 $PPRINT = true;
 
 
